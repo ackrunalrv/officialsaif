65d4f326a1389af2211ae8f08617d7ac2b812427
ackrunalrv <krunaltabiyad7@gmail.com>
2025-03-02 21:08:47 +0530

    add a script navbar

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
 
 // scroll sections
 let sections = document.querySelectorAll('section');
@@ -30,6 +34,8 @@ window.onscroll = () => {
 
     // remove toggle icon and navbar when click navbar links (scroll)
 
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 
     // animation footer on scroll
 
